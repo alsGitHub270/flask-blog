3311fb381925118b3f2cb589db56f080c286d461
Al_ <junkmail954@yahoo.com>
2017-03-27 20:42:02 +0000

    first commit

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object('settings')
+db = SQLAlchemy(app)
+
+#migrations
+migrate = Migrate(app, db)
+
+from blog import views
+from author import views
