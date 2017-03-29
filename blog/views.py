e6fd5b31655dae8bfd02b485822f9492d012763f
Al_ <junkmail954@yahoo.com>
2017-03-29 16:32:52 +0000

    End Lesson:64  Post and Category Models, and Post Migration and Testing

--- blog/views.py
+++ blog/views.py
@@ -57,5 +57,9 @@ def setup():
             db.session.rollback()
             error = "Error creating blog!"
         
-    
     return render_template('blog/setup.html', form=form, error=error)
+    
+@app.route('/post')
+@login_required
+def post():
+    return 'Blog Post'
