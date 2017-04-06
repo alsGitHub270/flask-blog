f6d26d9757c251be9793288e4c9c391bbbecc7f3
Al_ <junkmail954@yahoo.com>
2017-04-06 15:59:06 +0000

    Includes Post and Category Models, Post Migration, Markdown, and Blog Post Form (Through Lession 66)

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from flask_blog import app
 from flask import render_template, redirect, flash, url_for, session, abort
-from blog.form import SetupForm
+from blog.form import SetupForm, PostForm
 from flask_blog import db
 from author.models import Author
 from blog.models import Blog
-from author.decorators import login_required
+from author.decorators import login_required, author_required
 import bcrypt
 
 @app.route('/')
@@ -16,7 +16,7 @@ def index():
     return('Hello World!')
     
 @app.route('/admin')
-@login_required
+@author_required
 def admin():
     if session.get('is_author'):
         return render_template('blog/admin.html')
@@ -56,10 +56,14 @@ def setup():
         else:
             db.session.rollback()
             error = "Error creating blog!"
-        
     return render_template('blog/setup.html', form=form, error=error)
     
-@app.route('/post')
-@login_required
+@app.route('/post', methods=('GET', 'POST'))
+@author_required
 def post():
-    return 'Blog Post'
+    form = PostForm()
+    return render_template('blog/post.html', form=form)
+    
+@app.route('/article')
+def article():
+    return render_template('blog/article.html')
