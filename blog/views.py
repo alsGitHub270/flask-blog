1adde38e9018f8061e6f4bcf75a340c44b2169e5
Al_ <junkmail954@yahoo.com>
2017-04-13 19:31:14 +0000

    Lession 76:  Adding images, Display image, delete article, edit article.  End of First Flask Application(Flask Blog)

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,14 @@
 from flask_blog import app
-from flask import render_template, redirect, flash, url_for, session, abort
+from flask import render_template, redirect, flash, url_for, session, abort, request
 from blog.form import SetupForm, PostForm
-from flask_blog import db
+from flask_blog import db, uploaded_images
 from author.models import Author
 from blog.models import Blog, Post, Category
 from author.decorators import login_required, author_required
 import bcrypt
 from slugify import slugify
 
-POST_PER_PAGE = 3
+POST_PER_PAGE = 5
 
 @app.route('/')
 @app.route('/index')
@@ -17,7 +17,7 @@ def index(page=1):
     blog = Blog.query.first()
     if not blog:
         return redirect(url_for('setup'))
-    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page, POST_PER_PAGE, False)
+    posts = Post.query.filter_by(live=True).order_by(Post.publish_date.desc()).paginate(page, POST_PER_PAGE, False)
     return render_template('blog/index.html', blog=blog, posts=posts)
     
 @app.route('/admin')
@@ -70,6 +70,12 @@ def setup():
 def post():
     form = PostForm()
     if form.validate_on_submit():
+        image = request.files.get('image')
+        filename = None
+        try:
+            filename = uploaded_images.save(image)
+        except:
+            flash("The image was not uploaded")
         if form.new_category.data:
             new_category = Category(form.new_category.data)
             db.session.add(new_category)
@@ -85,13 +91,50 @@ def post():
         title = form.title.data 
         body = form.body.data
         slug = slugify(title)
-        post = Post(blog, author, title, body, category, slug)
+        post = Post(blog, author, title, body, category, filename, slug)
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('article', slug=slug))
-    return render_template('blog/post.html', form=form)
+    return render_template('blog/post.html', form=form, action="new")
     
 @app.route('/article/<slug>')
 def article(slug):
     post = Post.query.filter_by(slug=slug).first_or_404()
     return render_template('blog/article.html', post=post)
+  
+@app.route('/edit/<int:post_id>', methods=('GET','POST'))
+@author_required
+def edit(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    form = PostForm(obj=post)
+    if form.validate_on_submit():
+        original_image = post.image
+        form.populate_obj(post)
+        if form.image.has_file():
+            image = request.files.get('image')
+            try:
+                filename = uploaded_images.save(image)
+            except:
+                flash("The image was not uploaded!")
+            if filename:
+                post.image = filename
+        else:
+            post.image = original_image
+        if form.new_category.data:
+            new_category = Category(form.new_category.data)
+            db.session.add(new_category)
+            db.session.flush()
+            post.category = new_category
+        db.session.commit()
+        return redirect(url_for('article', slug=post.slug))
+    return render_template('blog/post.html', form=form, post=post, action="edit")
+  
+@app.route('/delete/<int:post_id>')
+@author_required
+def delete(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    post.live = False
+    db.session.commit()
+    flash("Article Deleted")
+    return redirect('/admin')
+
